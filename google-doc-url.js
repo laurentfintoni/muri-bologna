d816c57eabe92849c00b43e3590c0daf6fa9b65c
Laurent Fintoni <72798408+laurentfintoni@users.noreply.github.com>
2022-06-19 10:11:44 +0200

    update API key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1kB-8DUHOTVEA1qGvkInY4RgygMdXq-C2YiyAT4WsVuU/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = 'AIzaSyC7uBYkGkjiCqRJG2X-VmbMNSI1MJyUgIA';
