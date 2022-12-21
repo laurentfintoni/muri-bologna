f0c3ce610fea517f0d39543ba124c3a27fc594c9
Laurent Fintoni <72798408+laurentfintoni@users.noreply.github.com>
2022-12-21 09:54:16 -0800

    2022 updates

---
+++ google-doc-url_2022.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1s80tptgcRC0MMieypHPCA6SYXzSMso-QEpkLkuCUcsE/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyC7uBYkGkjiCqRJG2X-VmbMNSI1MJyUgIA';
